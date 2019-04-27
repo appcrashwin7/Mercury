9884eb3dce4ad2e713b32f1be9a0f56f5f17ef31
appcrashwin7 <xcv678@wp.pl>
2019-04-27 18:13:33 +0200

    add Product class and Commodities

---
+++ Mercury/Commodities.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <vector>
+
+#include "Product.h"
+
+class Commodities
+{
+	std::vector<Product> Commd;
+
+public:
+	Commodities()
+	{
+		Commd.push_back(Product("Water", 1.0f, ProductType::Resource));
+		Commd.push_back(Product("Heavy water", 1.1f, ProductType::Resource));
+		Commd.push_back(Product("Waste", 1.5f, ProductType::Dangerous));
+		Commd.push_back(Product("Steel", 7.8f, ProductType::Material));
+		Commd.push_back(Product("Stainless steel", 7.5f, ProductType::Material));
+		Commd.push_back(Product("Duraluminium", 2.7f, ProductType::Material));
+		Commd.push_back(Product("Electronics", 2.0f, ProductType::Parts));
+		Commd.push_back(Product("Industrial parts", 4.0f, ProductType::Parts));
+		Commd.push_back(Product("Rocket parts", 3.5f, ProductType::Parts));
+		Commd.push_back(Product("High-tech rocket parts", 3.0f, ProductType::Parts));
+		Commd.push_back(Product("Jewerly", 4.5f, ProductType::Material));
+		Commd.push_back(Product("Low-enriched uranium", 18.0f, ProductType::Radioactive));
+		Commd.push_back(Product("Kerosine-LOX", 1.0f, ProductType::Material));
+
+	}
+	Commodities(const Commodities & other) = delete;
+	~Commodities() = default;
+
+	const std::vector<Product> & get() const
+	{
+		return Commd;
+	}
+};
---
+++ Mercury/Product.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <string>
+
+enum class ProductType
+{
+	Resource,
+	Material,
+	Dangerous,
+	Gaseous,
+	Parts,
+	Electronic,
+	Organic,
+	Radioactive
+};
+
+class Product
+{
+	const std::string name;
+	const float massPerUnit;
+	const ProductType type;
+
+public:
+	Product() = delete;
+	Product(const Product & other)
+		:name(other.name), massPerUnit(other.massPerUnit), type(other.type)
+	{}
+	Product(std::string pname, float mass, ProductType t)
+		:name(pname), massPerUnit(mass), type(t)
+	{}
+	~Product() = default;
+
+	bool operator==(const Product & other) const
+	{
+		if (name == other.name)
+		{
+			if (type == other.type)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	const std::string & getName() const
+	{
+		return name;
+	}
+	float getMassPerUnit() const
+	{
+		return massPerUnit;
+	}
+	ProductType getType() const
+	{
+		return type;
+	}
+};
