89e03074a9ade5b23c1e08d3c21e8d5f08f4c59d
appcrashwin7 <xcv678@wp.pl>
2019-05-31 11:38:57 +0200

    add Units header

--- Mercury/Calc.h
+++ Mercury/Calc.h
@@ -5,26 +5,7 @@
 #include <cassert>
 #include <qstring.h>
 
-#include <boost/units/unit.hpp>
-#include <boost/units/systems/si.hpp>
-#include <boost/units/systems/cgs.hpp>
-
-namespace boost
-{
-	namespace units
-	{
-		static const auto megawatt = units::si::watt * 1000000;
-	};
-};
-
-namespace units = boost::units;
-using Length = units::quantity<units::si::length, double>;
-using Mass = units::quantity<units::si::mass, double>;
-using Acceleration = units::quantity<units::si::acceleration, float>;
-using Velocity = units::quantity<units::si::velocity, float>;
-using Energy = units::quantity<units::si::power, uint64_t>;
-using Temperature = units::quantity<units::si::temperature, int32_t>;
-using EnergyD = units::quantity<units::si::power, double>;
+#include "Units.h"
 
 
 static constexpr float GRAVITY_CONSTANT = 6.67e-11f;
---
+++ Mercury/Units.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <boost/units/unit.hpp>
+#include <boost/units/systems/si.hpp>
+#include <boost/units/systems/cgs.hpp>
+
+namespace boost
+{
+	namespace units
+	{
+		static const auto megawatt = units::si::watt * 1000000;
+	};
+};
+
+namespace units = boost::units;
+using Length = units::quantity<units::si::length, double>;
+using Mass = units::quantity<units::si::mass, double>;
+using Acceleration = units::quantity<units::si::acceleration, float>;
+using Velocity = units::quantity<units::si::velocity, float>;
+using Energy = units::quantity<units::si::power, uint64_t>;
+using Temperature = units::quantity<units::si::temperature, int32_t>;
+using EnergyD = units::quantity<units::si::power, double>;
