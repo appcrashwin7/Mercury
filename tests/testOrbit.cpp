3fde7c63c61174764358f9faf54c975327ba3184
appcrashwin7 <xcv678@wp.pl>
2019-06-11 22:36:44 +0200

    add additional test case for Orbit class

--- tests/testOrbit.cpp
+++ tests/testOrbit.cpp
@@ -11,4 +11,16 @@ TEST(Orbit_Class, DefaultConstructor)
 	ASSERT_EQ(orbit.isDefault, true);
 	ASSERT_EQ(orbit.apoapsis, Length());
 	ASSERT_EQ(orbit.periapsis, Length());
+	ASSERT_FLOAT_EQ(orbit.eccentricity, 0.0f);
+	ASSERT_EQ(orbit.getOrbitalPeriod(), TimeInt());
+}
+
+TEST(Orbit_Class, properCalculations)
+{
+	auto sunMass = units::si::kilogram * 1.9885e30;
+	Orbit earthOrb(units::si::meter * 1.521e11, units::si::meter * 1.47095e11, sunMass, 0);
+
+	ASSERT_NEAR(earthOrb.eccentricity,/*real earth orbit eccentricity*/ 0.0167086f, 0.0002);
+	auto orbitalPeriod = earthOrb.getOrbitalPeriod().value() / units::days.value();
+	ASSERT_EQ(orbitalPeriod, 365);
 }
