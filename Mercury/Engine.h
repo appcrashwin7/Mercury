a167aeb0dae33f2d4a5b6abf0e243e5a66b61adc
appcrashwin7 <xcv678@wp.pl>
2020-11-03 14:27:55 +0100

    move Engine fields + remove iostream lib (to match code style of proj)

--- Mercury/Engine.h
+++ Mercury/Engine.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <qdatetime>
-#include <iostream>
 
 #include "GameWindow.h"
 #include "Universe.h"
@@ -11,20 +10,18 @@ class Engine : public QObject
 {
 	Q_OBJECT;
 		
+	QString gameName;
+	QDateTime gameTime;
+	Universe gameUniverse;
+	GameWindow window;
 public:
 	Engine(QWidget * mainWindow, QString gameName);
 	Engine(QWidget * mainWindow, QString gameName, Universe universe, QDateTime time);
 	~Engine();
 
 private:
-	QString gameName;
-	QDateTime gameTime;
-	Universe gameUniverse;
-	GameWindow window;
-
-	void changeTime(TimeChange change = TimeChange::hour_1);
-
 	void init();
 
+	void changeTime(TimeChange change = TimeChange::hour_1);
 	void showBodyInfo(size_t bodyID);
 };
